502b9a3ad327f018d36be221f2366dac5aeba82d
eleventy7 <534336+eleventy7@users.noreply.github.com>
2023-02-06 09:45:38 -0500

    Checkstyle

--- cluster/src/main/java/io/aeron/samples/ClusterApp.java
+++ cluster/src/main/java/io/aeron/samples/ClusterApp.java
@@ -13,8 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Properties;
 
 import static java.lang.Integer.parseInt;
 
