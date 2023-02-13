8d882901d1f216670b1e3738120ca509e28f4443
eleventy7 <534336+eleventy7@users.noreply.github.com>
2023-02-13 13:30:27 -0500

    Tidy up

--- k8s_find_leader.sh
+++ k8s_find_leader.sh
@@ -1,9 +1,3 @@
-commands=(
-  "kubectl logs aeron-io-sample-cluster-0 -n aeron-io-sample-cluster"
-  "kubectl logs aeron-io-sample-cluster-1 -n aeron-io-sample-cluster"
-  "kubectl logs aeron-io-sample-cluster-2 -n aeron-io-sample-cluster"
-)
-
 nodes=(
   "aeron-io-sample-cluster-0"
   "aeron-io-sample-cluster-1"
