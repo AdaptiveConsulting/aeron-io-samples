eda6cc716e0cbdb98d165e5f736b0d5b17585a3d
Shaun L <534336+eleventy7@users.noreply.github.com>
2023-02-13 08:52:56 -0500

    Demo helper scripts (#23)

---
+++ k8s_find_leader.sh
@@ -0,0 +1,19 @@
+commands=(
+  "kubectl logs aeron-io-sample-cluster-0 -n aeron-io-sample-cluster"
+  "kubectl logs aeron-io-sample-cluster-1 -n aeron-io-sample-cluster"
+  "kubectl logs aeron-io-sample-cluster-2 -n aeron-io-sample-cluster"
+)
+
+nodes=(
+  "aeron-io-sample-cluster-0"
+  "aeron-io-sample-cluster-1"
+  "aeron-io-sample-cluster-2"
+)
+
+for i in {0..2}; do
+  output=$(${commands[i]} | tail -5)
+  if [[ $output == *"LEADER"* ]]; then
+    echo "${nodes[i]}"
+    break
+  fi
+done
