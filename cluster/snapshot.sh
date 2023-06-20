e17699b6af52a9cef8dd64fb2752f7c2429eec1d
eleventy7 <534336+eleventy7@users.noreply.github.com>
2023-06-20 09:18:34 -0400

    Explictly set Cluster base directory (issue #77)

--- cluster/snapshot.sh
+++ cluster/snapshot.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-clusterdir=$(ls -d */ | cut -f1 -d'/' | grep aeron-cluster | head -1)
-java -cp ~/aeron/aeron-all-*.jar io.aeron.cluster.ClusterTool ~/jar/$clusterdir/cluster snapshot
+java -cp ~/aeron/aeron-all-*.jar io.aeron.cluster.ClusterTool ~/jar/aeron-cluster/cluster snapshot
