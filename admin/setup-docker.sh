317d116a2605758087f63b4cc4985bc561f08171
eleventy7 <534336+eleventy7@users.noreply.github.com>
2023-11-11 14:54:01 -0500

    Run admin as aeron user

--- admin/setup-docker.sh
+++ admin/setup-docker.sh
@@ -8,10 +8,5 @@ apt-get install \
     --quiet \
     --assume-yes \
     --no-install-recommends \
-    bash \
-    wget
+    bash
 
-mkdir /root/aeron
-mkdir /root/jar
-
-wget https://repo1.maven.org/maven2/io/aeron/aeron-all/1.42.1/aeron-all-1.42.1.jar -P /root/aeron/
